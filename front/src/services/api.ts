5bb342c43a80b2682946a8ba708c2491a5504ca0
ignacio <ignacio.tumini@hotmail.com>
2024-08-28 09:46:59 -0300

    update api

--- front/src/services/api.ts
+++ front/src/services/api.ts
@@ -1,9 +1,5 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: "http://localhost:3000",
-});
-
-export const getPokemons = () => api.get("/pokemons");
+export const getPokemons = () => axios.get("/pokemons");
 export const battlePokemons = (pokemon1Id: string, pokemon2Id: string) =>
-  api.post("/battle", { pokemon1Id, pokemon2Id });
+  axios.post("/battle", { pokemon1Id, pokemon2Id });
