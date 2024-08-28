43415b78ff91a6e7b815f528fe4bcb3f56c94576
ignacio <ignacio.tumini@hotmail.com>
2024-08-28 15:32:22 -0300

    terminar migraciones

---
+++ back/src/data-source.ts
@@ -0,0 +1,18 @@
+import { DataSource } from 'typeorm';
+
+export const AppDataSource = new DataSource({
+  type: 'sqlite',
+  database: 'database.sqlite',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/migrations/*{.ts,.js}'],
+  synchronize: false,
+  logging: false,
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization', err);
+  });
