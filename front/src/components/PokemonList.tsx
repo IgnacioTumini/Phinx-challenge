a5b4d5357b864d2eacf48af5c5ef3ac450669942
ignacio <ignacio.tumini@hotmail.com>
2024-08-28 10:20:36 -0300

    create pokemon list

---
+++ front/src/components/PokemonList.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from "react";
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  Typography,
+  Avatar,
+} from "@mui/material";
+
+interface PokemonListProps {
+  pokemons: any[];
+  onSelect: (pokemon: any) => void;
+}
+
+const PokemonList: React.FC<PokemonListProps> = ({ pokemons, onSelect }) => {
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      flexWrap="wrap"
+      gap={2}
+      marginTop={2}
+    >
+      {pokemons.map((pokemon) => (
+        <Card
+          key={pokemon.id}
+          sx={{ width: 150 }}
+          onClick={() => onSelect(pokemon)}
+        >
+          <CardActionArea>
+            <CardContent sx={{ textAlign: "center" }}>
+              <Avatar
+                alt={pokemon.name}
+                src={pokemon.imageUrl}
+                sx={{ width: 80, height: 80, margin: "auto" }}
+              />
+              <Typography variant="h6">{pokemon.name}</Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      ))}
+    </Box>
+  );
+};
+
+export default PokemonList;
